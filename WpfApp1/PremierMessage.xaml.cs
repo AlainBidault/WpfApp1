3dceed41ceeeb4e33416be3cc5da7ce46ec2a141
ABI <alain@top.fr>
2019-12-13 15:57:27 +0100

    Ajoutez des fichiers projet.

---
+++ WpfApp1/PremierMessage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using Personnes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// https://codes-sources.commentcamarche.net/faq/11277-apercu-du-binding-en-wpf
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged // détecte le changement de propriété
+    {
+
+        private string prenom0 = "Jacques";
+        private string nom0 = "POMME";
+
+        private Personne qqn; // attribut utilisé dans un binding
+        public Personne Qqn {
+            get { return qqn; }
+            set { qqn = value; OnPropertyChanged("Qqn.Usage"); }// On appelle la méthode si le qqn change
+        }
+        public string Nom0
+        {
+            get { return nom0; }
+            set { nom0 = value; OnPropertyChanged("Nom0"); }// On appelle la méthode si le qqn change
+        }
+        public string Prenom0
+        {
+            get { return prenom0; }
+            set { prenom0 = value; OnPropertyChanged("Prenom0"); }// On appelle la méthode si le qqn change
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged; // implémentation de INotifyPropertyChanged
+
+        private void OnPropertyChanged(string nomPropriete)
+        {
+            if (PropertyChanged!=null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(nomPropriete));
+            }
+        }
+
+        public MainWindow()
+        {
+            qqn = new Personne();
+            InitializeComponent();
+            this.DataContext = this; // pour que les bindings fonctionnent
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if ((bool)OUI.IsChecked)
+            {
+                MessageBox.Show("Oui.");
+            }
+            else if ((bool)NON.IsChecked)
+            {
+                Nom0 = "Unknown";
+                Qqn.Nom = "inconnu";
+                MessageBox.Show("NONNON.");
+
+            }
+            else if ((bool)BS.IsChecked)
+            {
+                Prenom0 = "NotKnown";
+                Qqn.Prenom = "pas connu";
+                MessageBox.Show("BS.");
+            }
+        }
+    }
+}
---
+++ WpfApp1/Source/Modele/Personne.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personnes
+{
+    public class Personne : INotifyPropertyChanged
+    {
+        private string nom = "COURGE"; // attribut utilisé dans un binding
+        private string prenom = "Michel"; // attribut utilisé dans un binding
+
+        public string Prenom
+        {
+            get { return prenom; }
+            set { prenom = value; OnPropertyChanged("Qqn.Prenom"); }// On appelle la méthode si le nom change
+        }
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; OnPropertyChanged("Qqn.Nom"); }// On appelle la méthode si le nom change
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string nomPropriete)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(nomPropriete));
+            }
+        }
+
+        public List<String> LesEnfants()
+        {
+            List<String> rep = new List<string>(4);
+            rep.Add("Simon");
+            rep.Add("Timon");
+            rep.Add("Limon");
+            rep.Add("Pimon");
+            return rep;
+        }
+
+        public override string ToString()
+        {            
+            return Prenom+" "+Nom;
+        }
+
+    }
+}
